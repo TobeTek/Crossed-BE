2ce3a491658a9934c085dbd7ba8b8c7faa85fadc
TobeTek <katchyemma@gmail.com>
2021-07-26 00:35:31 +0100

    fixed maintenance issues

--- crossed_project/settings.py
+++ crossed_project/settings.py
@@ -10,18 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from settings.base import *
-
+from os import environ
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vwb8c)52qqvx)7qbvsb(#1rw@qwk=j_mj%0pc^ilr3zeu*1#!p'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
